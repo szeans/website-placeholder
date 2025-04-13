ec4bc4b0ca2a59b74014ae7f76ecd07437d39de5
Sean Choi <szeanchoi@gmail.com>
2025-04-12 22:53:43 -0700

    work

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  },
-  server: {
-    port: 3000
-  }
+  plugins: [react()]
 })
